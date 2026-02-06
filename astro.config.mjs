fe60d03c66a5019e80c8989ed3ef803000d89e38
Siow Meng Low <siowmeng.low@gmail.com>
2026-02-06 12:18:19 +0800

    Corrected astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://siowmenglow.github.io',
+  site: 'https://siowmenglow.com',
+  base: '/',
+  adapter: github(),
 });
